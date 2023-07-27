91a13381afe1b28bef0a5d8f4121db4fdc00d41a
Apurva <apurva.jpr@gmail.com>
2023-07-27 19:48:53 +0530

    Day 2

---
+++ java/day2/numPalindrome.java
@@ -0,0 +1,10 @@
+public class numPalindrome {
+  public static void main(String arg[]) {
+    int x = 12;
+    int sum = 0;
+    int index = 0
+    while (true) {
+      int digit = x % 10    
+    }
+  }
+}
